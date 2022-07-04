faa960ecbbf52d9db786f89bc1d7915173d32ede
jade_rabbit1124 <jeff13401769@gmail.com>
2022-07-04 20:10:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import random
+import asyncio
 
-for i in range(10):
-    a = random.randint(1,1000)
-    print(a)
+async def main():
+    i = 1
+    while i < 2:
+        a = random.randint(1,1000)
+        print(a)
+        await asyncio.sleep(3)
+
+asyncio.run(main())
